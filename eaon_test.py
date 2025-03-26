8d6ed2534824738911fa3deec04a9f2db013eb5d
Henrik <89130067+Va-ll-e@users.noreply.github.com>
2025-03-26 15:58:55 +0100

    Currently here

--- eaon_test.py
+++ eaon_test.py
@@ -1,9 +1,10 @@
 # Aeon requires python v.12
 # pip install aeon[all]
 from aeon.datasets import load_airline
+import numpy as np
+from aeon.anomaly_detection import STOMP
 
-y = load_airline()
+X = np.random.default_rng(42).random((10, 2), dtype=np.float64)
+detector = STOMP(X, window_size=2)
+detector.fit_predict(X, axis=0)
 
-print(len(y))
-
-print(y[:5])
